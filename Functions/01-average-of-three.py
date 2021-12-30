640c743fd1cb511606969c55ae6a05b988fcd4c2
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2021-12-30 03:40:01 +0000

    Add method exercises.

---
+++ Functions/01-average-of-three.py
@@ -0,0 +1,8 @@
+#Write a method average_of_three(num1, num2, num3) that returns the average of three numbers
+
+def average_of_three(num1, num2, num3):
+  return (num1 + num2 + num3) / 3
+
+print(average_of_three(3, 7, 8)) # => 6.0
+print(average_of_three(2, 5, 17)) # => 8.0
+print(average_of_three(2, 8, 1)) # => 3.666666
